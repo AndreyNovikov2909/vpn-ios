153ff21ab8a76be57638325970bba0dbe49c6969
Новиков Андрей Александрович <u15595>
2022-07-03 18:39:57 +0600

    feature(AMIOS-1): setup omportent modules

---
+++ Main/Main/Main.h
@@ -0,0 +1,18 @@
+//
+//  Main.h
+//  Main
+//
+//  Created by Alfauser on 30.06.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Main.
+FOUNDATION_EXPORT double MainVersionNumber;
+
+//! Project version string for Main.
+FOUNDATION_EXPORT const unsigned char MainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Main/PublicHeader.h>
+
+
---
+++ vpn-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vpn-ios
+//
+//  Created by Alfauser on 30.06.2022.
+//
+
+import UIKit
+import Networking
+import CommonUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+    }
+}
+
